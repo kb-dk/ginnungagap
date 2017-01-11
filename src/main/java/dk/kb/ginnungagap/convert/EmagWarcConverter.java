68fb5726cf55aedad422a50193240b3be6f4b353
Jonas Lindberg Frellesen <jolf@kb.dk>
2017-01-11 07:59:30 +0100

    Made the warc converter for Emagasinet. Needs testing

--- src/main/java/dk/kb/ginnungagap/convert/EmagWarcConverter.java
+++ src/main/java/dk/kb/ginnungagap/convert/EmagWarcConverter.java
@@ -1,6 +1,10 @@
 package dk.kb.ginnungagap.convert;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 import dk.kb.ginnungagap.archive.WarcPacker;
 import dk.kb.ginnungagap.config.Configuration;
@@ -30,8 +34,36 @@ public class EmagWarcConverter extends EmagConverter {
 
     @Override
     protected void handleRecord(CumulusRecord record, File contentFile) {
-        // TODO Auto-generated method stub
+        try {
+            record.initFields();
+            record.validateRequiredFields(conf.getTransformationConf().getRequiredFields());
 
+            File metadataFile = transformAndValidateMetadata(record);
+            packer.packRecord(record, metadataFile);
+        } catch (IOException e) {
+            throw new IllegalStateException("", e);
+        }
     }
 
+    
+    /**
+     * Transforms and validates the metadata from the Cumulus record.
+     * 
+     * @param record The record with the metadata to transform and validate.
+     * @return The file containing the transformed metadata.
+     * @throws IOException If an error occurs when reading or writing the metadata.
+     */
+    protected File transformAndValidateMetadata(CumulusRecord record) throws IOException {
+        String metadataUUID = record.getMetadataGUID();
+        File metadataFile = new File(conf.getTransformationConf().getMetadataTempDir(), metadataUUID);
+        try (OutputStream os = new FileOutputStream(metadataFile)) {
+            File cumulusMetadataFile = new File(conf.getTransformationConf().getMetadataTempDir(), metadataUUID + "_raw.xml");
+            transformer.transformXmlMetadata(record.getMetadata(cumulusMetadataFile), os);
+            os.flush();
+        }
+        
+        transformer.validate(new FileInputStream(metadataFile));
+        
+        return metadataFile;
+    }
 }
